e63224b963306d085ae79389483ee1661aabfa61
cosuoji <qelvinz@gmail.com>
2025-09-03 19:50:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,19 @@ const limiter = rateLimit({
 app.use('/api/', limiter);
 
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://sunny-beijinho-58fda2.netlify.app"
+];
+
 app.use(cors({
-  origin: "https://sunny-beijinho-58fda2.netlify.app/", // your Netlify URL
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
